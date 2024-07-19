b42688ca98e1cc00ccf8498ee59bfdbb33080842
Grace Roessling <grace.roessling19@gmail.com>
2024-07-19 13:42:36 -0400

    removed warning issue

--- experiment_analyses/execute_pipeline.py
+++ experiment_analyses/execute_pipeline.py
@@ -1,7 +1,9 @@
 import preprocess
-
+import warnings
 SUBJECT_PATH = "C:/Users/graci/Dropbox/PAndA/Thesis Experiment 1/data/main_data"
 TRACK_CENTER_PATH = "C:/Users/graci/Dropbox/PAndA/Thesis Experiment 1/data/track_piece_center"
 LANE_DEVIATION_PATH = "C:/Users/graci/Dropbox/PAndA/Thesis Experiment 1/data/lane_deviation_data"
 
+warnings.filterwarnings("ignore")
 subject_dict,master_dict,non_interp_dict = preprocess.run(SUBJECT_PATH, TRACK_CENTER_PATH, LANE_DEVIATION_PATH)
+
