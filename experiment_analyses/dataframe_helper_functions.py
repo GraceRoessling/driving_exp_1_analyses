d4eda30092f527abd06e0f45763d9205d0abcf50
Grace Roessling <grace.roessling19@gmail.com>
2025-04-01 16:40:17 -0400

    fixed plotting issue

--- experiment_analyses/dataframe_helper_functions.py
+++ experiment_analyses/dataframe_helper_functions.py
@@ -111,8 +111,8 @@ def remove_NA(cam_position_df,vehicle_position_df,driving_vars_df):
     return(cam_position_df,vehicle_position_df,driving_vars_df)
 
 def get_track_piece_indices(piece_object,cam_position_df,vehicle_position_df,driving_vars_df):
-    indices_for_track_piece = list(driving_vars_df.index[driving_vars_df['current_track_piece'].str.contains(piece_object.id)])
-    track_piece_driving_var = driving_vars_df[driving_vars_df['current_track_piece'].str.contains(piece_object.id)]   
+    indices_for_track_piece = list(driving_vars_df.index[driving_vars_df['current_track_piece'] == piece_object.id])
+    track_piece_driving_var = driving_vars_df[driving_vars_df['current_track_piece'] == piece_object.id]   
     first_index,last_index = indices_for_track_piece[0],indices_for_track_piece[-1]
     track_piece_cam_pos = cam_position_df.iloc[first_index:last_index+1]
     track_piece_vehicle_pos = vehicle_position_df.iloc[first_index:last_index+1]
