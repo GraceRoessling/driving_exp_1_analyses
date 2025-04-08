8c194e779c2e0e8f90376875bc6043e9ae9e1f11
Grace Roessling <grace.roessling19@gmail.com>
2025-04-08 11:49:58 -0400

    analyzing all subjects in exp 2

--- experiment_analyses/subject.py
+++ experiment_analyses/subject.py
@@ -4,8 +4,8 @@ class Subject:
     "This is the subject class. A subject is a person object that has performance information affiliated to their driving behavior."
     
     subject_id_dict = {
-    "familiar": ["baggy", "bash", "boned", "cargo","five", "grip", "mule", "polio", "rerun", "slate", "slept", "trial", "yeast"],
-    "unfamiliar": ["atom", "brim", "chef", "clerk", "debt", "grid", "lens", "limb", "most","slimy", "swarm", "wok"]
+    "familiar": ["baggy", "bash", "boned", "cargo","five", "grip","judge", "mule","poker","polio", "rerun", "slate", "slept", "trial", "yeast"],
+    "unfamiliar": ["atom","blank", "brim", "chef", "clerk", "debt","filth", "grid", "lens", "limb", "most","proof","slimy", "swarm", "wok"]
     }
 
     def __init__(self, id, condition, dir_path):
