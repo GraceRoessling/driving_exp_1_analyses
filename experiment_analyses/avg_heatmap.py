32f1930a33ae338d87589144eb923420407a3763
Grace Roessling <grace.roessling19@gmail.com>
2025-07-01 15:45:34 -0400

    avg heatmap params
    param tweaks

--- experiment_analyses/avg_heatmap.py
+++ experiment_analyses/avg_heatmap.py
@@ -92,7 +92,7 @@ def plot_centerline_speed_heatmap(condition,track_centerline_x, track_centerline
     speed_values = np.array([average_speed_dict.get(i, 0) for i in range(len(x_vals) - 1)])
 
     # Normalize speeds for color mapping
-    norm = plt.Normalize(vmin=0, vmax=30)
+    norm = plt.Normalize(vmin=0, vmax=25)
     cmap = cm.get_cmap("coolwarm")
 
     # Create LineCollection with average speeds
@@ -116,4 +116,4 @@ def plot_centerline_speed_heatmap(condition,track_centerline_x, track_centerline
     plt.grid(True)
     plt.tight_layout()
     #plt.show()
-    plt.savefig(f"C:/Users/graci/Dropbox/PAndA/Thesis Experiment 2/presentations/r_figures/{condition}_g_heatmap.svg")
+    plt.savefig(f"C:/Users/graci/Dropbox/PAndA/Thesis Experiment 3/presentations/r_analyses/{condition}_heatmap.svg")
