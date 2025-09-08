5bd4225c04571935f5d1f4fb65c8a0deef833dd9
Grace Roessling <grace.roessling19@gmail.com>
2025-09-07 20:53:45 -0400

    fixed dtw analysis for all subjects

--- experiment_analyses/spataial_knowledge_and_perf_analysis.py
+++ experiment_analyses/spataial_knowledge_and_perf_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-dtw_df = pd.read_csv("C:/Users/graci/Dropbox/PAndA/Thesis Experiment 2/data/dtw_per_subj.csv")
+dtw_df = pd.read_csv("C:/Users/graci/Dropbox/PAndA/Thesis Experiment 2/data/dtw_data_recovered.csv")
 drawing_scores_df = pd.read_csv("C:/Users/graci/Dropbox/PAndA/Thesis Experiment 2/data/drawing_scores.csv")
 
 print(dtw_df)
